29888b07b105776f802ce40aac2e62cea760b44f
Michael David Clark <clark.mike1@gmail.com>
2016-10-03 09:42:29 -0400

    removed a bunch of unneccessary code

--- app.R
+++ app.R
@@ -4,6 +4,7 @@ library(rsconnect)
 library(etable)
 library(tables)
 long <- read.csv("long.csv", header=TRUE)
+
 ui <- fluidPage(
   title="Cognitive and Neuropsychatric Data",
   sidebarPanel(
@@ -13,26 +14,16 @@ ui <- fluidPage(
       selectInput("value", "Choose a graph metric:", choices=c("Mean + SD"=1,"Number of Subjects in Strata"=2), selected =1)
       ),
   mainPanel(
-    tags$head( 
-        tags$style( HTML('#mytable table {border-collapse:collapse; } 
-                             #mytable table th { transform: rotate(-45deg)}'
-                                ))),
     tableOutput("mytable")
-    
-  ))
+    ))
 
 server <- function(input, output){
-x<-reactive(input$value)
-  
-
 output$mytable <- renderTable({
-    if(x()==1){
+    if(input$value==1){
     fun_value<-etable::mean_sd_cell
     }else{fun_value <-etable::n_cell}
   Tab <- tabular.ade(x_vars='RawScore', rows='Test', cols=input$columns, cnames=input$columns, data=long, FUN=fun_value)
-  xtable(Tab)
-  
-
+  Tab
 })
 } 
 shinyApp(ui = ui, server = server)
